9552f28cd15f340851e243849ab87dfd10347ddf
Juste Raimbault <juste.raimbault@polytechnique.edu>
2020-11-17 15:39:55 +0000

    scotland_pbf

--- OSM/Geofabrik/scotland/filterBuildings.sh
+++ OSM/Geofabrik/scotland/filterBuildings.sh
@@ -1,3 +1,3 @@
-osmosis --read-pbf $1 --log-progress --tf accept-ways building=* --used-node --write-xml $2
+osmosis --read-pbf $1 --log-progress --tf accept-ways building=* --used-node --write-pbf $2
 #osmosis --read-pbf $1 --log-progress --tf accept-ways building=* --write-xml $2
 
