aa2f24744897d87970034c26dfe09e3ab1212193
Juste Raimbault <juste.raimbault@polytechnique.edu>
2021-01-12 14:53:22 +0000

    convert_contours

---
+++ IGN/ContourIris/convert.sh
@@ -0,0 +1,5 @@
+#ogr2ogr -f ‘ESRI Shapefile’ -a_srs EPSG:4326 Output_fileName.shp Input_FileName.geojson
+cp contours-iris.geojson tmp.geojson
+ogr2ogr -f 'ESRI Shapefile' CONTOURS-IRIS.shp tmp.geojson -sql "SELECT insee_com AS INSEE_COM, code_iris AS CODE_IRIS from tmp"
+rm tmp.geojson
+
